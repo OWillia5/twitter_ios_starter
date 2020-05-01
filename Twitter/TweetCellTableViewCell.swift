bc23eb751c76d1c408d0ed430e44d2e3d28e5679
Opeoluwa Williams <davwilliams6@gmail.com>
2020-05-01 07:31:58 -0700

    Created login and homepage screens. Added features - load tweets, pull to refresh and infinite scroll

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by user169361 on 5/1/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweet: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
